ad2b701255cf295e83a4bf84669b1a8def0a0989
nosurlife <nosurlife@hotmail.com>
2018-11-17 18:22:57 +1300

    Initial commit

---
+++ Data/MemeApiContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MemeBank.Models;
+
+namespace MemeApi.Models
+{
+    public class MemeApiContext : DbContext
+    {
+        public MemeApiContext (DbContextOptions<MemeApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MemeBank.Models.MemeItem> MemeItem { get; set; }
+    }
+}
